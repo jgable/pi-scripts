0eb0abbe80b88c17e53c8ae6accb78c663ae018c
Jacob Gable <jacob.gable@gmail.com>
2013-12-24 13:00:06 -0600

    Fix for not sending array of data

--- test/i2cpart_spec.js
+++ test/i2cpart_spec.js
@@ -2,26 +2,50 @@
 
 var Part = require('../lib/i2cpart'),
 	should = require('should'),
+	sinon = require('sinon'),
 	_ = require('lodash');
 
 describe('I2CPart', function () {
-	var part;
+	var part,
+		sandbox;
 
 	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+
 		part = new Part({
 			address: 32
 		});
+	});
+
+	afterEach(function () {
+		sandbox.restore();
 	});
 
 	it('creates an i2c wire', function () {
 		should.exist(part.wire);
 	});
 
-	it('can read data', function () {
+	it('can read data', function (done) {
 		_.isFunction(part.read).should.equal(true);
+
+		var readSpy = sandbox.spy(part.wire, 'readBytes');
+
+		part.read(9, function () {
+			readSpy.called.should.equal(true);
+
+			done();
+		});
 	});
 
-	it('can write data', function () {
+	it('can send data', function (done) {
 		_.isFunction(part.send).should.equal(true);
+
+		var writeSpy = sandbox.spy(part.wire, 'writeBytes');
+
+		part.send(9, 1, function () {
+			writeSpy.called.should.equal(true);
+
+			done();
+		});
 	});
 });
